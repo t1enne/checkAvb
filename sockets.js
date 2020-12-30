75688ab8c84776a974031e0be211eef22862eee9
t1enne <nasir.taov@gmail.com>
2020-12-30 22:44:17 +0100

    'added collab feature'

---
+++ sockets.js
@@ -0,0 +1,19 @@
+const socketio = require('socket.io')
+
+function init(server) {
+  const io = socketio(server)
+
+  io.on('connection', socket => {
+    io.emit('message-client-connected', `Client with id ${socket.id} was connected`)
+
+    socket.on('input', item => {
+      //item has id, field and value
+      io.emit(`remote-input`, item)
+    })
+
+  });
+
+}
+
+
+module.exports = init
