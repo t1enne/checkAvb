bf5f5756eb3478708e24472d18bc7e5f8cc6a162
t1enne <nasir.taov@gmail.com>
2020-12-31 09:48:51 +0100

    'regex sockets'

--- public/app.js
+++ public/app.js
@@ -2,7 +2,9 @@ import 'regenerator-runtime/runtime';
 import m from 'mithril';
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3000')
+const socket = io('http://localhost:3000', {
+  withCredentials: true
+})
 
 
 socket.on('connect', () => {})
